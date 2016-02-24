02508da0d1d51075d8faf70f1da6f6e6db8f9515
OneTimeCZ <zed97@seznam.cz>
2016-02-24 22:05:49 +0100

    info page data wip

--- controllers/InfoController.php
+++ controllers/InfoController.php
@@ -4,16 +4,28 @@ namespace Controllers;
 
 use Models\Article;
 use Models\ArticleQuery;
+use Models\User;
+use Models\UserQuery;
 
 class InfoController extends Controller{
 
     public function index(){
-        //SQL
+        $participants = UserQuery::create()
+            ->joinWith("Member")
+            ->joinWith("Image")
+            ->find();
+        
+        $parents_faq = array(
+            array("Co to vlastně je?", "Cow landjaeger pork chop pork loin prosciutto beef ribs turkey. Porchetta beef ribs sausage, picanha short ribs rump boudin spare ribs landjaeger. Sausage shank landjaeger rump prosciutto short ribs."),
+            array("Kolik to stojí?", "Salami doner strip steak leberkas, ham hock flank alcatra shank. Pig rump hamburger doner venison. Fatback meatball sirloin, chicken pancetta picanha pig swine alcatra rump ground round jowl pork belly.")
+        );
         
         $this->view('Info/index', 'base_template', [
             'active' => 'info',
             'title' => 'Informace',
-            'recent' => ArticleQuery::recent()
+            'recent' => ArticleQuery::recent(),
+            'participants' => $participants,
+            'parents_faq' => $parents_faq
         ]);
     }
 }
